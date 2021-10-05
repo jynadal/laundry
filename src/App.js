a52ddf744e0cb932963cf76ca8a7fa2f5a47b017
jynadal <nadaljeanyves@hotmail.fr>
2021-10-05 10:40:26 +0200

    commit before build and V1 deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 
 import Navbar from "./components/Navbar";
 import Testimonial from "./components/Testimonial";
